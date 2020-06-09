9211556af9ab3121e93b82534392fecdc092e399
timelord <timemonster666@gmail.com>
2020-06-09 20:47:06 +0500

    modes

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/client/index.js',
     module: {
         rules: [
