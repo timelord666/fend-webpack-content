780e9f84633e02f5c2514cc1b6aa5619de32faf7
timelord <timemonster666@gmail.com>
2020-06-10 21:58:54 +0500

    updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,38 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
-        rules: [{
-            test: '/\.js$/',
-            exclude: '/node_modules/',
-            loader: 'babel-loader'
-        }]
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+    ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html',
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
     ]
 }
