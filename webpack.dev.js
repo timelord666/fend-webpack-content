0a7e71a46e21c1a0d0309ce3ea1346e511a813e9
timelord <timemonster666@gmail.com>
2020-06-10 17:24:52 +0500

    clean plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
@@ -25,6 +26,7 @@ module.exports = {
             verbose: true,
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        
     ]
 }
