3dfe5cb79415e84040c34a660ec458c577308eb6
timelord <timemonster666@gmail.com>
2020-06-10 22:13:38 +0500

    api challenge complete

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,9 +6,15 @@ export function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=$100097&appid=$fbea738c9a1bf72c2d1a29fdaa2a304a')
+        .then(res => res.json())
+        .then(res => {
+           document.getElementById('results').innerHTML = res.message;
+        })
+
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
 }
